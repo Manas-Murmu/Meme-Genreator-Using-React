3a112d2cd1a5e3c5b20f290f49f3d953250b1d84
Manas Ranjan <manasranjanmurmu@gmail.com>
2020-08-29 10:00:42 +0530

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MemeGenerator from "./MemeGenerator";
+import HeaderComponent from "./HeaderComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <HeaderComponent />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeaderComponent() {
+  return (
+    <div id="hd">
+      <h1>Meme Generator</h1>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/03/Troll-Face-Meme-PNG.png"
+        alt="Problem"
+      />
+    </div>
+  );
+}
+
+export default HeaderComponent;
